7b77faa0fbd67c098fd12cb6e3376d2dd88e1476
linconvidal <linconvidal@gmail.com>
2020-08-05 20:46:08 -0300

    remove reply_templates-EN and update app.py

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 from credentials import bot_token, wallet_url
+from reply_templates import *
 from telegram.ext import Updater, CommandHandler
 import logging
 import requests
-import json
-from io import StringIO
+import random
 
 
-# Set URL default value
-url = wallet_url
+def help_callback(update, context):
+    update.message.reply_html(help_reply[language])
+
+def change_lang_callback(update, context):
+    global language
+
+    # Extract language ID from message
+    if context.args:
+        user_lang = ' '.join(context.args).upper()
+        if user_lang in supported_languages:
+            language = user_lang
+            update.message.reply_html(change_lang_reply[language])
+        else:
+            update.message.reply_html(f"I don't speak {user_lang} yet 😞")
+
+    else:
+        while True:
+            random_lang = random.choice(supported_languages)
+            if random_lang != language:
+                language = random_lang
+                break
+
+        update.message.reply_html(change_lang_reply[language])
+
 
 def netinfo_callback(update, context):
     target = '/network/information'
@@ -21,19 +43,7 @@ def netinfo_callback(update, context):
 
 
 def poolinfo_callback(update, context):
-    html_reply = """
-<b>Pool [{}] {}</b>
-<i>{}</i>
-    pledge: {} ada
-    cost: {} ada
-    margin: {}%
-
-<b>Metrics</b>
-    saturation: {}
-    controlled stake: {}%
-    produced blocks: {}
-    rewards: {} ada
-"""
+    update.message.reply_html(poolinfo_reply_wait[language])
 
     target = '/stake-pools?stake=0'
     r = requests.get(url+target)
@@ -49,34 +59,57 @@ def poolinfo_callback(update, context):
     lov = 1000000
 
     for pool in json_data:
-        if 'metadata' in pool:
-            if pool['metadata']['ticker']==ticker:
-                gotpool = True
-                pool_name = pool['metadata']['name']
-                desc = pool['metadata']['description']
-                site = pool['metadata']['homepage']
-                pledge = pool['pledge']['quantity']
-                pledge_ada = str(int(pledge /lov))
-                cost = pool['cost']['quantity']
-                cost_ada = str(int(cost/lov))
-                margin_perc = pool['margin']['quantity']
-                saturat = pool['metrics']['saturation']
-                rel_stake_perc = pool['metrics']['relative_stake']['quantity']
-                blocks = pool['metrics']['produced_blocks']['quantity']
-                rewards = pool['metrics']['non_myopic_member_rewards']['quantity']
-                rewards_ada = str(int(rewards/lov))
-                update.message.reply_html(html_reply.format(ticker, pool_name,desc,pledge_ada,cost_ada,margin_perc,saturat,rel_stake_perc,blocks,rewards_ada))
-                break
+        if 'metadata' in pool and pool['metadata']['ticker'] == ticker:
+            gotpool = True
+            pool_name = pool['metadata']['name']
+            desc = pool['metadata']['description']
+            site = pool['metadata']['homepage']
+            pledge = pool['pledge']['quantity']
+            pledge_ada = str(int(pledge /lov))
+            cost = pool['cost']['quantity']
+            cost_ada = str(int(cost/lov))
+            margin_perc = pool['margin']['quantity']
+            saturat = pool['metrics']['saturation']
+            rel_stake_perc = pool['metrics']['relative_stake']['quantity']
+            blocks = pool['metrics']['produced_blocks']['quantity']
+            rewards = pool['metrics']['non_myopic_member_rewards']['quantity']
+            rewards_ada = str(int(rewards/lov))
+            update.message.reply_html(
+                poolinfo_reply[language].format(
+                    quote=True,
+                    ticker=ticker,
+                    pool_name=pool_name,
+                    desc=desc,
+                    pledge_ada=pledge_ada,
+                    cost_ada=cost_ada,
+                    margin_perc=margin_perc,
+                    saturat=saturat,
+                    rel_stake_perc=rel_stake_perc,
+                    blocks=blocks,
+                    rewards_ada=rewards_ada))
+            break
 
     if not gotpool:
-        update.message.reply_text(f"Sorry, I didn't find the {ticker} pool :(")
+        update.message.reply_html(poolinfo_reply_error[language])
 
 
 def start_callback(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi, I'm a bot")
+    update.message.reply_html(welcome_reply[language])
 
 
 if __name__ == '__main__':
+    # Global language definitions
+    global supported_languages
+    global language
+
+    # Set URL default value
+    url = wallet_url
+
+    # Define languages
+    supported_languages = ['EN','PT']
+    language = 'PT'
+
+    # set telegram object
     updater = Updater(bot_token, use_context=True)
     dispatcher = updater.dispatcher
 
@@ -94,5 +127,14 @@ if __name__ == '__main__':
     # poolinfo handler
     poolinfo_handler = CommandHandler('poolinfo', poolinfo_callback)
     dispatcher.add_handler(poolinfo_handler)
+
+    # language handler
+    language_handler = CommandHandler('language', change_lang_callback)
+    dispatcher.add_handler(language_handler)
+
+    # help handler
+    help_handler = CommandHandler('help', help_callback)
+    dispatcher.add_handler(help_handler)
+
 
     updater.start_polling()
