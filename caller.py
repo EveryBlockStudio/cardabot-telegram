03f885024dbdf098f680a853ad34428bdfdbcf6d
mpds <matheusmullerps@gmail.com>
2022-01-05 18:58:04 -0300

    refactor and format

--- caller.py
+++ caller.py
@@ -1,24 +1,27 @@
+from typing import Dict
 from dotenv.main import load_dotenv
 from sgqlc.endpoint.http import HTTPEndpoint
 from pathlib import Path
 import os
 
-# Get data as dictionary from EBS's private cardano graphql endpoint based on the query filename
-# stored under src/queries/ directory
-def getDataFromCardanoNode(queryName, variables={}):
-    load_dotenv(override=True)
-    url = os.environ.get("GRAPHQL_URL")
-    # get query file path and read the query as bytes
+
+def get_data_from_node(query_name: str, variables={}) -> Dict:
+    """Request data from EBS's private cardano graphql endpoint.
+
+    Query text is obtained from file `query_name` stored under the `src/queries/` dir.
+
+    """
     try:
         query = Path(
-            Path(__file__).parent, "src", "query", f"{queryName}.graphql"
-        ).read_bytes()
+            Path(__file__).parent, "src", "query", f"{query_name}.graphql"
+        ).read_bytes()  # get query file path and read the query as bytes
+
     except FileNotFoundError:
-        print(f"Query file {queryName} not found")
+        print(f"Query file {query_name} not found")  # TODO: maybe use logging instead?
         return None
 
     # create endpoint object and execute query with given variables
-    endpoint = HTTPEndpoint(url)
+    endpoint = HTTPEndpoint(os.environ.get("GRAPHQL_URL"))
     data = endpoint(query, variables)
 
     # returns data as dictionary
@@ -27,5 +30,6 @@ def getDataFromCardanoNode(queryName, variables={}):
 
 if __name__ == "__main__":
     # sample query for getting stake pool details about EBS stake pool
+    load_dotenv(override=True)
     EBS_pool = "pool1ndtsklata6rphamr6jw2p3ltnzayq3pezhg0djvn7n5js8rqlzh"
-    print(getDataFromCardanoNode("stakePoolById", {"id": EBS_pool}))
+    print(get_data_from_node("stakePoolById", {"id": EBS_pool}))
