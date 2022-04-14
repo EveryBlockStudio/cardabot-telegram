7cc0fcf9684c648b1411d02eeb6dc8a300a6c0c2
Lincon Vidal <linconvidal@gmail.com>
2022-04-14 10:50:53 -0300

    Implement cli argument to run in production (webhook instead of pooling)

--- cardabot_telegram/app.py
+++ cardabot_telegram/app.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import argparse
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler
 
@@ -37,12 +38,21 @@ if __name__ == "__main__":
     dispatcher.add_handler(CommandHandler("netparams", cbs.netparams))
     dispatcher.add_handler(CommandHandler("netstats", cbs.netstats))
 
-    # start bot with pooling (use when running local)
-    updater.start_polling()
-
-    # Start bot with webhook (use in production)
-    # updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=BOT_TOKEN)
-    # updater.bot.setWebhook(APP_DOMAIN + BOT_TOKEN)
+    # parse command line arguments and start the bot accordingly
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prod", help="Run in production mode", action="store_true")
+    args = parser.parse_known_args()
+    
+    if args[0].prod: 
+        # start bot with webhook (use in production)
+        updater.start_webhook(
+            listen="0.0.0.0", 
+            port=int(os.environ.get("PORT")), 
+            url_path=os.environ.get("BOT_TOKEN"))
+        updater.bot.setWebhook(os.environ.get("APP_DOMAIN") + os.environ.get("BOT_TOKEN"))
+    else:
+        # start bot with pooling (use when running local)
+        updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or
     # SIGABRT.
